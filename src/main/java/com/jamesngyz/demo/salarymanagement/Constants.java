4070914a7699a90f3bfa865f16a9878969f570fc
James Ng <hello@jamesngyz.com>
2021-06-24 02:38:47 +0800

    Implement UserRepository createOrUpdate query; Create employee table

--- src/main/java/com/jamesngyz/demo/salarymanagement/Constants.java
+++ src/main/java/com/jamesngyz/demo/salarymanagement/Constants.java
@@ -3,5 +3,6 @@ package com.jamesngyz.demo.salarymanagement;
 public final class Constants {
 	
 	public static final String DATE_FORMAT = "dd-MMM-yy";
+	public static final String DATE_FORMAT_SQL = "yyyy-MM-dd";
 	
 }
--- src/main/java/com/jamesngyz/demo/salarymanagement/user/UserRepository.java
+++ src/main/java/com/jamesngyz/demo/salarymanagement/user/UserRepository.java
@@ -1,9 +1,13 @@
 package com.jamesngyz.demo.salarymanagement.user;
 
+import java.text.SimpleDateFormat;
 import java.util.List;
+import java.util.StringJoiner;
 
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Repository;
+
+import com.jamesngyz.demo.salarymanagement.Constants;
 
 @Repository
 public class UserRepository {
@@ -14,8 +18,25 @@ public class UserRepository {
 		this.jdbcTemplate = jdbcTemplate;
 	}
 	
-	Integer createOrUpdate(List<User> requested) {
-		return null;
+	Integer createOrUpdate(List<User> request) {
+		String query = "INSERT INTO employee (id, login, name, salary, start_date) VALUES \n";
+		StringJoiner rowsJoiner = new StringJoiner(",\n");
+		for (User u : request) {
+			StringJoiner fieldsJoiner = new StringJoiner("','", "('", "')");
+			fieldsJoiner
+					.add(u.getId())
+					.add(u.getLogin())
+					.add(u.getName())
+					.add(u.getSalary().toString())
+					.add(new SimpleDateFormat(Constants.DATE_FORMAT_SQL).format(u.getStartDate()));
+			rowsJoiner.add(fieldsJoiner.toString());
+		}
+		query = query + rowsJoiner.toString() +
+				" ON DUPLICATE KEY UPDATE " +
+				" id=VALUES(id), login=VALUES(login), name=VALUES(name), salary=VALUES(salary), start_date=VALUES(start_date);";
+		
+		// TODO: For POC purposes only. Will change this to parameter binding.
+		return jdbcTemplate.update(query);
 	}
 	
 }
